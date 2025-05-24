7185f2c650fe468c10743a50b78d2e80a82f3b15
Mt-Rasmus <rasmus.stahl.47@gmail.com>
2025-05-24 11:04:25 +0200

    add routing for single video entry page

---
+++ src/components/VideoEntry.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const videoId = route.params.id;
+
+</script>
+
+<template>
+  <div class="flex justify-center w-full">
+    <div class="wrapper">
+      <div>Heyyyy dis a video Entry page</div>
+      <div>{{ `And the ID of the video is ${videoId}` }}</div>
+      <div class="youtube-video">
+        <iframe
+          width="560"
+          height="315"
+          :src="'https://www.youtube.com/embed/aRIY36bBk78'"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.wrapper {
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,10 @@ import VideosList from '../components/VideosList.vue'
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col items-center">
+    <div class="my-6">
+      Waddup. How's everyone doin. You are now in the world I'm ruling.
+    </div>
     <VideosList />
   </div>
 </template>
