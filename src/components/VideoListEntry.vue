73fa149c2740e4140a4e75c1f51550e98071e4d0
Mt-Rasmus <rasmus.stahl.47@gmail.com>
2025-05-24 08:18:17 +0200

    cleanup project

---
+++ src/components/VideoListEntry.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  video: {
+    thumbnail: string;
+    gifPreview: string;
+  };
+}>();
+</script>
+
+<template>
+  <div class="w-[320px] h-[180px] bg-amber-600 border cursor-pointer relative overflow-hidden">
+    <img
+      :src="video.thumbnail"
+      class="absolute inset-0 w-full h-full object-cover transition-opacity duration-300 hover:opacity-0"
+    />
+    <img
+      :src="video.gifPreview"
+      class="absolute inset-0 w-full h-full object-cover opacity-0 transition-opacity duration-300 hover:opacity-100"
+    />
+  </div>
+</template>
---
+++ src/components/VideosList.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+
+import VideoEntry from './VideoListEntry.vue';
+
+const videos = [
+  {
+    id: 1,
+    url: 'https://youtu.be/xJMvZjDDcuo?si=uJBjRBRN5uysVsil',
+    thumbnail: 'src/assets/thumbnails/SteveDogsThumbnail9.png',
+    gifPreview: 'https://media.giphy.com/media/abc123/giphy.gif',
+  },
+  {
+    id: 2,
+    url: 'https://youtu.be/xJMvZjDDcuo?si=uJBjRBRN5uysVsil',
+    thumbnail: 'src/assets/thumbnails/SteveDogsThumbnail9.png',
+    gifPreview: 'https://media.giphy.com/media/abc123/giphy.gif',
+  },
+  {
+    id: 3,
+    url: 'https://youtu.be/xJMvZjDDcuo?si=uJBjRBRN5uysVsil',
+    thumbnail: 'src/assets/thumbnails/SteveDogsThumbnail9.png',
+    gifPreview: 'https://media.giphy.com/media/abc123/giphy.gif',
+  },
+  {
+    id: 4,
+    url: 'https://youtu.be/xJMvZjDDcuo?si=uJBjRBRN5uysVsil',
+    thumbnail: 'src/assets/thumbnails/SteveDogsThumbnail9.png',
+    gifPreview: 'https://media.giphy.com/media/abc123/giphy.gif',
+  },
+]
+
+</script>
+
+<template>
+  <div>All dem videos n that</div>
+  <div class="flex justify-center w-full">
+    <div class="wrapper">
+      <div v-for="video in videos" :key="video.id">
+        <VideoEntry :video /> 
+      </div> 
+    </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+.wrapper {
+  display: grid;
+  grid-template-columns: minmax(33%, auto) minmax(33%, auto)
+}
+
+</style>
