d9225cae51568b1edcae5c15537688a85417c920
Mt-Rasmus <rasmus.stahl.47@gmail.com>
2025-06-16 23:08:23 +0200

    styling update, all head images

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,11 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="flex justify-center mt-5">
-      <nav class="flex justify-center">
+    <div class="flex flex-row justify-center items-center mt-5 px-5 w-full">
+      <div class="flex justify-start w-full">
+        <img src="/src/assets/header/charsLEFT.png" class="w-[350px]"/>
+      </div>
+      <nav class="flex justify-center items-center text-4xl font-bold">
         <div class="pr-4">
           <RouterLink to="/" class="!text-black">Home</RouterLink>
         </div>
@@ -13,6 +16,9 @@ import { RouterLink, RouterView } from 'vue-router'
           <RouterLink to="/about" class="!text-black">About</RouterLink>
         </div>       
       </nav>
+      <div class="flex justify-end w-full">
+        <img src="/src/assets/header/charsRIGHT.png" class="w-[350px]"/>
+      </div>
     </div>
   </header>
 
