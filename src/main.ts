2d992cb958b11d5e7643750728ceac578787cc2b
Mt-Rasmus <rasmus.stahl.47@gmail.com>
2026-01-30 16:01:05 +0100

    fix issue running with vuedevtools

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,29 @@
-import './assets/main.css';
+import './assets/main.css'
 
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+import 'element-plus/dist/index.css'
 
-import App from './App.vue';
-import router from './router';
+import App from './App.vue'
+import router from './router'
 
 // Create app first
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(createPinia());
-app.use(router);
-app.use(ElementPlus);
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus)
+// app.use(vueDevTools)
 
 // Mount the app after restoring the route
 router.isReady().then(() => {
-  const redirectPath = sessionStorage.getItem('redirectPath');
+  const redirectPath = sessionStorage.getItem('redirectPath')
   if (redirectPath) {
-    sessionStorage.removeItem('redirectPath');
-    router.replace(redirectPath); // 👈 key change here
+    sessionStorage.removeItem('redirectPath')
+    router.replace(redirectPath) // 👈 key change here
   }
 
-  app.mount('#app');
-});
+  app.mount('#app')
+})
--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 // import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-// base: '/rasmustgo/',
+  // base: '/rasmustgo/',
   base: '/',
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
+    // vueDevTools(),
     // tailwindcss(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
